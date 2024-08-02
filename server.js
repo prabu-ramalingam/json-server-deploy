0d9ffab7015b298df2a0a055a6603d853d669a29
prabu-ramalingam <60443784+prabu-ramalingam@users.noreply.github.com>
2024-08-02 11:36:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500;
 
+// Use CORS middleware to allow requests from any origin
+server.use(cors());
+
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
